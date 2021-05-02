693d4bacf78329fc2ba007b0d1dc97232ca08175
ddelarco <ddelarcorodri88@gmail.com>
2021-05-02 11:48:39 +0200

    Configuracion desde controlador utilizando Value

--- springboot-servicio-item/src/main/java/com/example/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/example/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.example.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,12 +22,17 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
+	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class); 
 
 	//@Qualifier especifica cual se quiere seleccionar por defecto
 	@Autowired
-//	@Qualifier("serviceFeign")
-	@Qualifier("serviceRestTemplate")
+	@Qualifier("serviceFeign")
+//	@Qualifier("serviceRestTemplate")
 	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(){
@@ -43,5 +55,14 @@ public class ItemController {
 		producto.setPrecio(60.00);
 		item.setProducto(producto);
 		return item;
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfiguracion(@Value("${server.port}") String puerto){
+		log.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 }
