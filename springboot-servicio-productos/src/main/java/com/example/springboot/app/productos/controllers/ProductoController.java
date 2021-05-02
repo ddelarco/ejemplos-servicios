d8fdc77ba208c51875dc4d0b3d825774657f1630
ddelarco <ddelarcorodri88@gmail.com>
2021-05-02 20:15:06 +0200

    Implementacion handlers PUT y DELETE en controlador de servicio productos

--- springboot-servicio-productos/src/main/java/com/example/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/example/springboot/app/productos/controllers/ProductoController.java
@@ -6,9 +6,11 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,10 +58,28 @@ public class ProductoController {
 		return producto;
 	}
 	
-	//Mostrara un 201 y se creara correctamente
+	//HttpStatus.CREATED mostrara un 201 y se creara correctamente
 	@PostMapping("/crear")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Producto crear(@RequestBody Producto producto) {
 		return productoServices.save(producto);
+	}
+	
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		Producto productoDb = productoServices.findById(id);
+		
+		productoDb.setName(producto.getName());
+		productoDb.setPrecio(producto.getPrecio());
+		
+		return productoServices.save(productoDb);
+	}
+	
+	//NO_CONTENT devolvera un 204 y se elimina un registro
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id) {
+		productoServices.deleteById(id);
 	}
 }
