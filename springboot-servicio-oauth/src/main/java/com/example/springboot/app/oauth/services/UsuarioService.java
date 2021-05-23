9dfdaee13cf0020cc7ac73b1237885191fdd2a95
ddelarco <ddelarcorodri88@gmail.com>
2021-05-23 18:25:26 +0200

    Manejo de Error 404 en componente de servicio UserDetailsService

--- springboot-servicio-oauth/src/main/java/com/example/springboot/app/oauth/services/UsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/example/springboot/app/oauth/services/UsuarioService.java
@@ -17,33 +17,35 @@ import org.springframework.stereotype.Service;
 import com.example.springboot.app.oauth.clients.UsuarioFeignClient;
 import com.example.springboot.app.usuarios.commons.models.entity.Usuario;
 
+import feign.FeignException;
 
 @Service
-public class UsuarioService implements IUsuarioService, UserDetailsService{
+public class UsuarioService implements IUsuarioService, UserDetailsService {
 
 	private Logger log = LoggerFactory.getLogger(UserDetailsService.class);
-	
+
 	@Autowired
 	private UsuarioFeignClient client;
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Usuario usuario = client.findByUsername(username);
-		
-		if (usuario == null) {
+
+		try {
+			Usuario usuario = client.findByUsername(username);
+
+			List<GrantedAuthority> authorities = usuario.getRoles().stream()
+					.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+					.peek(authority -> log.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
+
+			log.info("Usuario autenticado: " + username);
+
+			return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
+					authorities);
+		} catch (FeignException e) {
 			log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
-			throw new UsernameNotFoundException("Error en el login, no existe el usuario '" + username + "' en el sistema");
+			throw new UsernameNotFoundException(
+					"Error en el login, no existe el usuario '" + username + "' en el sistema");
 		}
-		
-		List<GrantedAuthority> authorities = usuario.getRoles()
-				.stream()
-				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
-				.peek(authority -> log.info("Role: " + authority.getAuthority()))
-				.collect(Collectors.toList());
-		
-		log.info("Usuario autenticado: "+ username);
-		
-		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
 	}
 
 	@Override
